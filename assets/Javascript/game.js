9281af526c4ac9488e401ba57dea0dbecc9a02be
Chris Gomes <gomezchris937@gmail.com>
2019-02-28 08:18:34 -0500

    works

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,14 +1,29 @@
-$(function() {
-    console.log("rand");
+var myNumber = Math.floor(Math.random()*101)+19
+$("#myNumber").text(myNumber)
+var counter=0;
 
-var random = Math.floor(Math.random()* 101 +19); 
+var red =Math.floor(Math.random()*11)+1
+var blue =Math.floor(Math.random()*11)+1
+var green =Math.floor(Math.random()*11)+1
+var yellow =Math.floor(Math.random()*11)+1
 
-var chris1 = Math.floor(Math.random()* 11+1);
-var chris2= Math.floor(Math.random()* 11+1);
-var chris3 = Math.floor(Math.random()* 11+1)
-var chris4= Math.floor(Math.random()* 11+1)
-var total= 0;
-var wins = 0;
-var losses= 0; 
+$("#myScore").text(counter);
 
- });
+$("#blue").on("click",function(){
+    counter+=blue;
+    $("#myScore").text(counter);
+    
+})
+
+$("#green").on("click",function(){
+    counter+=green;
+    $("#myScore").text(counter);
+})
+$("#red").on("click",function(){
+    counter+=red;
+    $("#myScore").text(counter);
+})
+$("#yellow").on("click",function(){
+    counter+=yellow;
+    $("#myScore").text(counter);
+})
